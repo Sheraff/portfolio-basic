e555335590daf8e33e076640142bce7a5d91f676
Sheraff <fpellet@ensc.fr>
2021-08-21 22:25:06 +0200

    no anim, pure css, correct responsive

--- components/SplitContainer.js
+++ components/SplitContainer.js
@@ -39,7 +39,7 @@ export default class SplitContainer extends HTMLElement {
 		this.state = false
 
 		// both standard DOM and cloned DOM events go through shadowRoot
-		this.shadowRoot.addEventListener('split-toggle', this.handle.bind(this))
+		this.shadowRoot.addEventListener('split-toggle', this.toggle.bind(this))
 	}
 
 	// make yourself known to global registry when added to DOM
@@ -71,68 +71,7 @@ export default class SplitContainer extends HTMLElement {
 		return null
 	}
 
-	handle(e) {
-
-		console.log(SplitContainer.findOpenInstance())
-		
-		if(e.detail) {
-			this.state = true
-			this.open(e.target)
-			this.split.appendChild(range.createContextualFragment(e.detail))
-			const delta = this.split.offsetHeight
-			const animOptions = {
-				duration: 500,
-				easing: 'ease',
-				fill: /** @type {FillMode} */('both'),
-			}
-			this.movables = findAllNextSiblings(this, [])
-			const {top} = this.movables[0].getBoundingClientRect()
-			const {bottom} = this.getBoundingClientRect()
-			const deltaToNextSibling = top - bottom
-			if (deltaToNextSibling > delta)
-				this.movables.length = 0
-			this.movables.forEach(movable => 
-				movable.animate([
-					{ transform: 'translateY(0px)' },
-					{ transform: `translateY(${delta - deltaToNextSibling}px)` },
-				], animOptions)
-			)
-			this.clone.animate([
-				{ transform: 'translateY(0px)' },
-				{ transform: `translateY(${delta}px)` },
-			], animOptions)
-			this.split.animate([
-				{ clipPath: `inset(0 0 ${delta}px 0)` },
-				{ clipPath: 'inset(0 0 0 0)' }
-			], animOptions)
-				.finished
-				.then(() => {
-					const scroll = window.scrollY
-					this.split.focus()
-					window.scrollTo(0, scroll)
-					this.style.removeProperty('pointer-events')
-				})
-			this.style.setProperty('pointer-events', 'none')
-		} else {
-			this.onClose()
-			this.movables.forEach(movable => 
-				movable.getAnimations().forEach(anim => {
-					anim.reverse()
-				})
-			)
-			this.clone.getAnimations().forEach(anim => {
-				anim.reverse()
-			})
-			this.split.getAnimations().forEach(anim => {
-				anim.reverse()
-				anim.finished.then(() => {
-					this.state = false
-					this.split.innerHTML = ''
-				})
-			})
-		}
-	}
-
+	// duplicate content into shadowRoot to give illusion of splitting it into two
 	makeClone() {
 		this.clone = document.createElement('div')
 		this.clone.classList.add('clone')
@@ -141,6 +80,7 @@ export default class SplitContainer extends HTMLElement {
 		this.shadowRoot.appendChild(this.clone)
 	}
 
+	// create slot into which to insert the content to show inside the split
 	makeSplit() {
 		this.split = document.createElement('div')
 		this.split.setAttribute('slot', 'split')
@@ -148,14 +88,126 @@ export default class SplitContainer extends HTMLElement {
 		this.appendChild(this.split)
 	}
 
-	open(target) {
+	toggle(e) {
+		if(e.detail) {
+			this.open(e.target, e.detail)
+		} else {
+			this.close()
+		}
+	}
+
+	/**
+	 * @param {import('./SplitTarget').default} target
+	 * @param {string} domString
+	 */
+	async open(target, domString) {
+		console.log()
+
+		const otherOpenInstance = SplitContainer.findOpenInstance()
+		if (otherOpenInstance) {
+			await otherOpenInstance.close()
+		}
+		
+		// update state and DOM
+		this.state = true
+		this.split.appendChild(range.createContextualFragment(domString))
+
+		// create listeners and variables
+		const {
+			delta,
+			toSibling,
+		} = this.setCss(target)
+
+		// apply transition animations
+		// const animOptions = {
+		// 	duration: 500,
+		// 	easing: 'ease',
+		// 	// fill: /** @type {FillMode} */('both'),
+		// 	fill: 'none',
+		// }
+		
+		// this.movables.forEach(movable => 
+		// 	movable.animate([
+		// 		{ transform: 'translateY(0px)' },
+		// 		{ transform: `translateY(${toSibling}px)` },
+		// 	], animOptions)
+		// )
+		// this.clone.animate([
+		// 	{ transform: 'translateY(0px)' },
+		// 	{ transform: `translateY(${delta}px)` },
+		// ], animOptions)
+		// this.split.animate([
+		// 	{ clipPath: `inset(0 0 ${delta}px 0)` },
+		// 	{ clipPath: 'inset(0 0 0 0)' }
+		// ], animOptions)
+		// 	.finished
+		// 	.then(() => {
+		// 		const scroll = window.scrollY
+		// 		this.split.focus()
+		// 		window.scrollTo(0, scroll)
+		// 		this.style.removeProperty('pointer-events')
+		// 	})
+		// this.style.setProperty('pointer-events', 'none')
+	
+	}
+
+	/**
+	 * @param {import('./SplitTarget').default} target
+	 */
+	setCss(target) {
+		this.movables = findAllNextSiblings(this, [])
+		this.movables.forEach(movable =>
+			movable.style.setProperty('transform', `translateY(var(--to-sibling, 0px))`)
+		)
+
 		const onResize = () => {
+			// where split-container content is split
 			const {top} = target.parentElement.getBoundingClientRect()
 			const {bottom} = target.getBoundingClientRect()
 			this.style.setProperty('--split', `${bottom - top}px`)
+
+			// how much space is occupied by the content
+			const delta = this.split.offsetHeight
+			this.clone.style.setProperty('--delta', `${delta}px`)
+
+			// how much space is left before the first sibling
+			const siblingTop = this.movables[0].offsetTop
+			const toSibling = Math.max(0, this.offsetTop + this.offsetHeight + delta - siblingTop)
+			document.body.style.setProperty('--to-sibling', `${toSibling}px`)
+
+			return {delta, toSibling}
 		}
-		onResize()
 		window.addEventListener('resize', onResize)
-		this.onClose = () => window.removeEventListener('resize', onResize)
+		this.unsetCss = () => {
+			this.movables.forEach(movable => movable.style.removeProperty('transform'))
+			this.clone.style.removeProperty('--delta')
+			document.body.style.removeProperty('--to-sibling')
+			window.removeEventListener('resize', onResize)
+		}
+		return onResize()
+	}
+
+	/**
+	 * @returns {Promise<void>} resolves when animation is complete
+	 */
+	close() {
+		// this.movables.forEach(movable => 
+		// 	movable.getAnimations().forEach(anim => {
+		// 		anim.reverse()
+		// 	})
+		// )
+		// this.movables.length = 0
+		// this.clone.getAnimations().forEach(anim => {
+		// 	anim.reverse()
+		// })
+		// this.split.getAnimations().forEach(anim => {
+		// 	anim.reverse()
+		// 	anim.finished.then(() => {
+				this.state = false
+				this.split.innerHTML = ''
+		// 	})
+		// })
+		this.unsetCss()
+		return Promise.resolve()
 	}
 }
